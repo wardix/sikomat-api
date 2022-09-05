d5cb22832626fe572e168df21afaab959faadd95
Wardi <wardi@nusa.net.id>
2022-09-05 09:09:08 +0700

    add config database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,24 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: process.env.DB_TYPE as 'mysql' | 'mariadb' | 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DBNAME,
+      entities: [],
+      synchronize: false
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
